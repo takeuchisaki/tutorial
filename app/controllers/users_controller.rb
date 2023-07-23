74ba48368945ef2d28fbf3b7c2d80e772e6e06a5
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-07-23 09:41:11 +0000

    Finish11章

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
  def index
     @users = User.paginate(page: params[:page])
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
@@ -18,11 +18,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
-      render :new
+      render 'new'
     end
   end
 
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
@@ -64,7 +64,7 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
-    
+
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
