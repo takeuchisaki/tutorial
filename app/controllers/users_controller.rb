6f946ecc1119efe036cf3d1efa6faefcb6a48329
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-07-25 09:32:02 +0000

    Finish13章

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,13 +53,13 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_path
-      end
-    end
+    # def logged_in_user
+    #   unless logged_in?
+    #     store_location
+    #     flash[:danger] = "Please log in."
+    #     redirect_to login_path
+    #   end
+    # end
 
     def correct_user
       @user = User.find(params[:id])
